02965d2566a5bb4178911e5d35ea63cd05a22f51
Kılıç Ali Temiz <katemiz@gmail.com>
2022-09-05 19:08:34 +0300

    Completed footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,6 @@
 <x-app-layout>
 
-    <style>
 
-        .herobg {
-            background-color: #b2d23a;
-        }
-    </style>
 
     <section class="hero herobg">
         <div class="hero-body">
@@ -15,6 +10,10 @@
             <div class="column is-7">
                 <p class="title">Documents in <span class="has-text-white">HTML</span></p>
                 <p class="subtitle">Your Docs, Your Freedom</p>
+
+                <p class="has-text-dark mt-6 pt-6">
+                    Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.
+                </p>
             </div>
 
             <div class="column">
